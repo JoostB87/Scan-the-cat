6d0b766f9eaca049f15d00f96ece23239e3bc65b
JoostB87 <joostbraakman@gmail.com>
2022-07-16 14:47:46 +0200

    DisplayCatResult

--- app/src/main/java/com/catapp/scanthecat/DisplayCatResultActivity.java
+++ app/src/main/java/com/catapp/scanthecat/DisplayCatResultActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -24,8 +25,18 @@ public class DisplayCatResultActivity extends AppCompatActivity {
         TextView txtCatName = findViewById(R.id.txtCatName);
         TextView txtCatOrigin = findViewById(R.id.txtCatOrigin);
         TextView txtCatLength = findViewById(R.id.txtCatLength);
-        TextView txtCatFamilyFriendly = findViewById(R.id.txtCatFamilyFriendly);
-        TextView txtCatChildFriendly = findViewById(R.id.txtCatChildFriendly);
+        TextView txtCatWeight = findViewById(R.id.txtCatWeight);
+        TextView txtCatLifeExpectancy = findViewById(R.id.txtCatLifeExpectancy);
+        RatingBar ratingFamilyFriendly = findViewById(R.id.ratingFamilyFriendly);
+        RatingBar ratingChildrenFriendly = findViewById(R.id.ratingChildrenFriendly);
+        RatingBar ratingStrangerFriendly = findViewById(R.id.ratingStrangerFriendly);
+        RatingBar ratingOtherPetsFriendly = findViewById(R.id.ratingOtherPetsFriendly);
+        RatingBar ratingGeneralHealth = findViewById(R.id.ratingGeneralHealth);
+        RatingBar ratingIntelligence = findViewById(R.id.ratingIntelligence);
+        RatingBar ratingPlayfulness = findViewById(R.id.ratingPlayfulness);
+        RatingBar ratingMeowing = findViewById(R.id.ratingMeowing);
+        RatingBar ratingShedding = findViewById(R.id.ratingShedding);
+        RatingBar ratingGrooming = findViewById(R.id.ratingGrooming);
 
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
@@ -40,8 +51,18 @@ public class DisplayCatResultActivity extends AppCompatActivity {
         txtCatName.setText(cats.getName());
         txtCatOrigin.setText(cats.getOrigin());
         txtCatLength.setText(cats.getLength());
-        txtCatFamilyFriendly.setText(String.valueOf(cats.getFamily_friendly()));
-        txtCatChildFriendly.setText(String.valueOf(cats.getChildren_friendly()));
+        txtCatWeight.setText(cats.getMin_weight() + " - " + cats.getMax_weight());
+        txtCatLifeExpectancy.setText(cats.getMin_life_expectancy() + " - " + cats.getMax_life_expectancy());
+        ratingFamilyFriendly.setRating(cats.getFamily_friendly());
+        ratingChildrenFriendly.setRating(cats.getChildren_friendly());
+        ratingStrangerFriendly.setRating(cats.getStranger_friendly());
+        ratingOtherPetsFriendly.setRating(cats.getOther_pets_friendly());
+        ratingGeneralHealth.setRating(cats.getGeneral_health());
+        ratingIntelligence.setRating(cats.getIntelligence());
+        ratingPlayfulness.setRating(cats.getPlayfulness());
+        ratingMeowing.setRating(cats.getMeowing());
+        ratingShedding.setRating(cats.getShedding());
+        ratingGrooming.setRating(cats.getGrooming());
 
     }
 }
